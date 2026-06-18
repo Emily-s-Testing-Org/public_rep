4d2596959feb3fc0ca33d21c532a1ef593447cbe
Emily Wise <wiseemily88@github.com>
2026-06-18 11:29:12 -0700

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -1,6 +1,10 @@
 class Ruby
 
-def enqueue_request_pull_request_reviewers_job(actor: user, should_re_request_reviews: false)
-    RequestPullRequestReviewersJob.perform_later(self, actor, should_re_request_reviews: should_re_request_reviews)
-  end
+    def enqueue_request_pull_request_reviewers_job(actor: user, should_re_request_reviews: false)
+        RequestPullRequestReviewersJob.perform_later(self, actor, should_re_request_reviews: should_re_request_reviews)
+    end
+
+    def dequeue_request_pull_request_reviewers_job(actor: user)
+        StopPullRequestReviewersJob.perform_later(self, actor)
+    end
 end
